c4c117fe3a3c7867dc31dfda91156e6f6c9a8924
rony-hassan <ronyhassan.cse@gmail.com>
2025-09-26 22:38:36 +0600

    animatio added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import assets from "../assets/assets";
 import Title from "./Title";
 import ServiceCard from "./ServiceCard";
+import { motion } from "motion/react";
 
 const Services = () => {
   const servicesData = [
@@ -31,7 +32,11 @@ const Services = () => {
   ];
 
   return (
-    <div
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      transition={{ staggerChildren: 0.2 }}
+      viewport={{ once: true }}
       id="services"
       className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
@@ -51,7 +56,7 @@ const Services = () => {
           <ServiceCard key={index} service={service} index={index} />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
